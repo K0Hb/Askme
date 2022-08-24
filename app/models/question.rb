101775b8ded096a3ab4fe466779ebdbf660f43f8
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-08-24 19:26:36 +0300

    del unnecessary method

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,17 +5,6 @@ class Question < ApplicationRecord
   belongs_to :author, class_name: 'User', optional: true
   has_and_belongs_to_many :hashtags
 
-  # before_save :find_hashtags
-
   validates :body, presence: true, length: { maximum: MAX_TEXT_LENGTH }
 
-  # def find_hashtags
-  #   hashtags = (body + (answer.nil? ? '' : answer)).scan(/#\p{L}+/).map(&:downcase).uniq
-  #   all_hashtags = Hashtag.all.map(&:name)
-  #   hashtags.each do |tag|
-  #     unless all_hashtags.include?(tag)
-  #       hashtag = Hashtag.create(name: tag).save
-  #     end
-  #   end
-  # end
 end
