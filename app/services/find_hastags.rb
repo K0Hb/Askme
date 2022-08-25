4ae13dba80fffc5769880d7034a4a249ea002c6a
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-08-25 21:03:16 +0300

    fib bug tag body + answer

--- app/services/find_hastags.rb
+++ app/services/find_hastags.rb
@@ -10,7 +10,7 @@ class FindHastags
   end
 
   def find_and_create_hastag
-    hashtags = (@question.body + (@question&.answer.nil? ? '' : @question.answer)).scan(REGEX_HASHTAG).map(&:downcase).uniq
+    hashtags = (@question.body + " " + (@question&.answer.nil? ? '' : @question.answer)).scan(REGEX_HASHTAG).map(&:downcase).uniq
     hashtags.each do |tag|
       hashtag = Hashtag.find_or_create_by(name: tag)
       unless @question.hashtags.include?(hashtag)
