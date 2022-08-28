0103ba1f3fc4f722564bb0a124c5ba3db728a34a
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-08-28 22:07:09 +0300

    add method delete_independent_hashtags

--- app/services/find_hastags.rb
+++ app/services/find_hastags.rb
@@ -11,11 +11,19 @@ class FindHastags
 
   def find_and_create_hastag
     hashtags = (@question.body + " " + (@question&.answer.nil? ? '' : @question.answer)).scan(REGEX_HASHTAG).map(&:downcase).uniq
+    delete_independent_hashtags(hashtags)
     hashtags.each do |tag|
       hashtag = Hashtag.find_or_create_by(name: tag)
       unless @question.hashtags.include?(hashtag)
         @question.hashtags << hashtag
       end
+    end
+  end
+
+  def delete_independent_hashtags(hashtags)
+    independent_hashtags = @question.hashtags.pluck(:name) - hashtags
+    independent_hashtags.each do |hashtag|
+      @question.hashtags.find_by_name(hashtag).delete
     end
   end
 end
