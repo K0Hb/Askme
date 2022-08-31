9f065671647baa4b515204b2314f92f3baf04712
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-08-31 12:13:23 +0300

    refactoring

--- app/services/find_hastags.rb
+++ app/services/find_hastags.rb
@@ -6,25 +6,13 @@ class FindHastags
   end
 
   def self.call(*args)
-    new(*args).find_and_create_hastag
+    new(*args).find_and_create_hashtag
   end
 
-  def find_and_create_hastag
-    hashtags = (@question.body + " " + (@question&.answer.nil? ? '' : @question.answer)).scan(REGEX_HASHTAG).map(&:downcase).uniq
-    delete_independent_hashtags(hashtags, @question)
-    hashtags.each do |tag|
-      hashtag = Hashtag.find_or_create_by(name: tag)
-      unless @question.hashtags.include?(hashtag)
-        @question.hashtags << hashtag
+  def find_and_create_hashtag
+    @question.hashtags =
+      "#{@question.body} #{@question.answer}".downcase.scan(REGEX_HASHTAG).uniq.map do |tag|
+        Hashtag.find_or_create_by(name: tag)
       end
-    end
-  end
-
-  def delete_independent_hashtags(hashtags, question)
-    independent_hashtags = @question.hashtags.pluck(:name) - hashtags
-    independent_hashtags.each do |hashtag|
-      hashtag_id = Hashtag.find_by_name(hashtag)
-      QuestionHashtag.where(question_id: hashtag_id, hashtag_id: question.id).first.delete
-    end
   end
 end
