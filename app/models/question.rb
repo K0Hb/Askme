d5888ea230cf84900bafc02c3ccb61e572559af8
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-09-03 16:04:00 +0300

    add validate user_id_not_change

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,4 +7,14 @@ class Question < ApplicationRecord
   has_many :hashtags, through: :question_hashtags, source: :hashtag
 
   validates :body, presence: true, length: { maximum: MAX_TEXT_LENGTH }
+
+  validate :user_id_not_changed
+
+  private
+
+  def user_id_not_changed
+    if user_id_changed? && self.persisted?
+      errors.add(:user_id, "Нельзя менять назначение вопроса")
+    end
+  end
 end
